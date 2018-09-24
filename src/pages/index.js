614dccf2b9bb78e64085465016c5fc901fee0fb4
Ben Robertson <benjamingrobertson@me.com>
2018-09-24 13:11:21 -0400

    Fix styling.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,51 +2,50 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 
-import Footer from '../components/footer/footer'
+import Layout from '../components/layout'
 import PostListing from '../components/post-listing/post-listing'
-import Header from '../components/header/header'
 
 import styles from './index.module.css'
 
 const Index = ({ data }) => {
   const jobs = data.allWordpressWpJobs.edges
   return (
-    <div className="index-container">
-      <Helmet
-        title={'Front End Remote Jobs'}
-        meta={[
-          {
-            name: 'description',
-            description: 'Fully remote jobs for front end developers.',
-          },
-        ]}
-      />
-      <Header />
-      <div className={styles.container}>
-        {jobs
-          .filter(({ node }) => {
-            const thirtyDaysAgo = new Date(
-              new Date().setDate(new Date().getDate() - 30)
-            )
-            // Only show dates from 30 days ago and up.
-            return new Date(node.date) > thirtyDaysAgo
-          })
-          .map(({ node }) => (
-            <PostListing
-              key={node.id}
-              post={{
-                title: node.title,
-                path: node.acf.apply_url,
-                company: node.acf.company,
-                snippet: node.excerpt,
-                date: node.posted,
-                slug: node.slug,
-              }}
-            />
-          ))}
-        <Footer />
+    <Layout>
+      <div className="index-container">
+        <Helmet
+          title={'Front End Remote Jobs'}
+          meta={[
+            {
+              name: 'description',
+              description: 'Fully remote jobs for front end developers.',
+            },
+          ]}
+        />
+        <div className={styles.container}>
+          {jobs
+            .filter(({ node }) => {
+              const thirtyDaysAgo = new Date(
+                new Date().setDate(new Date().getDate() - 30)
+              )
+              // Only show dates from 30 days ago and up.
+              return new Date(node.date) > thirtyDaysAgo
+            })
+            .map(({ node }) => (
+              <PostListing
+                key={node.id}
+                post={{
+                  title: node.title,
+                  path: node.acf.apply_url,
+                  company: node.acf.company,
+                  snippet: node.excerpt,
+                  date: node.posted,
+                  slug: node.slug,
+                }}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 

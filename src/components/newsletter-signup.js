fa37f1f746ba09a1d77d3a60c73d78331c3365bc
Ben Robertson <benjamingrobertson@me.com>
2018-10-06 14:47:29 -0400

    Update text.

--- src/components/newsletter-signup.js
+++ src/components/newsletter-signup.js
@@ -5,7 +5,7 @@ import styles from './newsletter-signup.module.css'
 
 const NewsletterSignup = () => (
   <div className={styles.formContainer}>
-    <p>Get remote jobs delivered to your inbox biweekly.</p>
+    <p>Get remote jobs delivered to your inbox weekly.</p>
     <MailchimpSubscribe url="https://benjamingrobertson.us15.list-manage.com/subscribe/post?u=aafc0f8e65dbc564446043b15&amp;id=ee47f8c168" />
   </div>
 )

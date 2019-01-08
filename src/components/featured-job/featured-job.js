653356d350d9c926353ce91754c9185793d4a5a0
Ben Robertson <benjamingrobertson@me.com>
2019-01-08 18:01:03 -0500

    Add initial featured job component.

---
+++ src/components/featured-job/featured-job.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import striptags from 'striptags';
+import styles from './featured-job.module.css';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import { Link } from 'gatsby';
+
+const FeaturedJob = ({ post }) => {
+  const title = `${
+    post.title
+    // TODO: this should be css.
+  } <span style="font-weight: normal;font-family: 'Lora', sans-serif; font-style: italic; text-transform: lowercase; font-size: 0.64em">at</span><br /> ${
+    post.company
+  }`;
+  return (
+    <div
+      aria-label={`${post.title} at ${post.company}`}
+      key={post.date + post.title}
+    >
+      <span className={styles.label}>Featured Job</span>
+      <div>
+        <h2 dangerouslySetInnerHTML={{ __html: title }} />
+      </div>
+
+      {post.snippet && (
+        <div>
+          <p dangerouslySetInnerHTML={{ __html: striptags(post.snippet) }} />
+        </div>
+      )}
+      <div>
+        <Link to={`/jobs/${post.slug}`}>Apply Now</Link>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedJob;

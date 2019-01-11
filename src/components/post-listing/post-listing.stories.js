ec8136fdafd5001bc98cc5db98b982af910b0abd
Ben Robertson <benjamingrobertson@me.com>
2019-01-11 17:05:10 -0500

    Add storybook.

---
+++ src/components/post-listing/post-listing.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import '../layout/layout.css';
+import PostListing from './post-listing';
+
+storiesOf(`PostListing`, module).add(`default`, () => (
+  <PostListing
+    post={{
+      title: 'Front End Developer',
+      company: 'Mediacurrent',
+      snippet:
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit senectus accumsan hendrerit penatibus velit tempus luctus a viverra fermentum dapibus, nascetur massa morbi condimentum nibh ultrices id proin feugiat scelerisque fusce curae tellus sollicitudin lobortis orci.',
+    }}
+  />
+));

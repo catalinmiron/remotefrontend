c04dba5d1b429abc79f3fd362b8d886e31b4218a
Ben Robertson <benjamingrobertson@me.com>
2019-01-14 21:50:48 -0500

    Add a new footer.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,28 +1,22 @@
 import React from 'react';
+import { Link } from '@reach/router';
+import Nav from '../nav/nav';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
+
 import styles from './footer.module.scss';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const { location } = this.props;
-
-    this.state = {
-      sticky: location && location.pathname === '/',
-    };
-  }
-
-  render() {
-    return (
-      <div className={this.state.sticky ? styles.sticky : ''}>
-        <footer className={styles.footer}>
-          <a href="mailto:hi@frontendremotejobs.com?subject=I'd like to post a job on frontendremotejobs.com">
-            Post a Job: $15 / month!
-          </a>
-        </footer>
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <footer className={styles.footer} aria-label="Footer">
+    <Nav />
+    <p>
+      <Link to="/">Front end remote jobs</Link> is a curated job board featuring
+      the best remote jobs for front end developers.
+    </p>
+    <p>
+      Made with ❤ by{' '}
+      <OutboundLink href="https://benrobertson.io">Ben Robertson</OutboundLink>
+    </p>
+  </footer>
+);
 
 export default Footer;

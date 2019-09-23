76b8b7857b87900e15cf20ea6e5d7656c320c5f6
Ben Robertson <benjamingrobertson@me.com>
2019-09-23 11:05:56 -0400

    Adds smallchat to post a job page.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Layout from './src/components/layout/layout';
+import Helmet from 'react-helmet';
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
+};
+
+export const onRenderBody = (
+  { pathname, setPostBodyComponents },
+  pluginOptions
+) => {
+  // Adds smallchat to post-a-job page only.
+  if (pathname.includes('post-a-job')) {
+    setPostBodyComponents([
+      <Helmet>
+        <script src="https://embed.small.chat/TNFEUQAFKGNMTA93NZ.js" async />
+      </Helmet>
+    ]);
+  }
 };
